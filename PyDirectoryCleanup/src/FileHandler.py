775e718925f8d3045f4dacc1487c466ea4c36cc0
clarkstoner <clarkstoner@protonmail.com>
2022-08-12 18:24:07 -0700

    Config Update
    Updated config setting to read from user input instead of using hardcoded items. last step is to create config generator to create user specified items.

--- PyDirectoryCleanup/src/FileHandler.py
+++ PyDirectoryCleanup/src/FileHandler.py
@@ -2,14 +2,14 @@ from genericpath import exists
 from watchdog.events import LoggingEventHandler
 from PyDirectoryCleanup.src.FileMover import FileMover
 import configparser
+import ast
 import os
 
 config_file = configparser.ConfigParser()
 config_file.read("./conf.ini")
 
 source_dir = config_file["source_dir"]["directory_to_watch"]
-dest_dir_videos = config_file["source_dir"]["directory_to_watch"]
-dest_dir_images = config_file["source_dir"]["directory_to_watch"]
+
 
 class FileHandler(LoggingEventHandler):
     def on_modified(self, event):
@@ -17,9 +17,7 @@ class FileHandler(LoggingEventHandler):
             for item in items:
                 name = item.name
                 dest = source_dir
-                if name.endswith(".jpg") or name.endswith(".png"):
-                    dest = dest_dir_images
-                    FileMover.move_file_to_new_dest(dest, item, name)
-                elif name.endswith(".mov") or name.endswith(".mp4") or name.endswith(".mkv"):
-                    dest = dest_dir_videos
-                    FileMover.move_file_to_new_dest(dest, item, name)
+                for move_type in config_file["move_to_id"]:
+                    if name.endswith(tuple(ast.literal_eval(config_file["move_to_types"][str(move_type)]))):
+                        dest = config_file["move_to_dir"][str(move_type)]
+                        FileMover.move_file_to_new_dest(dest, item, name)
